702d543be1f4541ef25c653e022f2cb7188a740b
ezmgszi <zaclipperd@gmail.com>
2022-07-11 02:11:30 -0500

    added error handling to API call

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-# imports
+# api_call
 import requests
 
 
-# api_call
 def api_call(ss, ct, ts, bd, ed):
     tsf = ""
-    if ts == 1:
+    interval_value = ''
+    if ts == "1":
         tsf = "TIME_SERIES_INTRADAY"
-    elif ts == 2:
+        interval_value = 'interval=5min'
+    elif ts == "2":
         tsf = "TIME_SERIES_DAILY"
-    elif ts == 4:
+    elif ts == "4":
         tsf = "TIME_SERIES_WEEKLY"
     else:
         tsf = "TIME_SERIES_MONTHLY"
-    url = 'https://www.alphavantage.co/query?function=' + tsf + '&symbol=' + ss + '&interval=5min&apikey=2C4AFL520Q27QCZ9'
+    url = 'https://www.alphavantage.co/query?function=' + tsf + '&symbol=' + ss + '&' + interval_value \
+          + '&apikey=2C4AFL520Q27QCZ9'
     print(url)
     r = requests.get(url)
     data = r.json()
+    if 'Error Message'in data :
+        print("There was an error in your request, please try again.")
+        return 1
 
     print(data)
+    return 0
+
 
 # exit_prompt
 def exit_prompt():
@@ -36,13 +43,19 @@ def exit_prompt():
 if __name__ == "__main__":
 
     while 1:
-        stock_symbol = "GOOGL"
+        stock_symbol = "Googl"
         chart_type = "2"
-        time_series = "2"
+        time_series = "1"
         begin_date = "1994-08-01"
         end_date = "1994-08-31'"
 
-        api_call(stock_symbol, chart_type, time_series, begin_date, end_date)
-        if exit_prompt() == 0:
-            break
+        ec = api_call(stock_symbol, chart_type, time_series, begin_date, end_date)
+        if ec == 0:
+            if exit_prompt() == 0:
+                break
+        else:
+            print("This is a testing point, normally loop but dont want to lock")
+            if exit_prompt() == 0:
+                break
+
 

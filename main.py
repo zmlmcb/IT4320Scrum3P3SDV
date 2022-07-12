6e146df655b476a56a24ba6d53bdf5cac26c55d3
ezmgszi <zaclipperd@gmail.com>
2022-07-11 23:31:51 -0500

    Transfer json object into a pandas df

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 # api_call
+import json
+
 import requests
+import datetime
+import pandas as pd
+
 
 
 def retrieve_date_range(data, bd, ed):
-    data_ranged = data
-    return data_ranged
+    if bd == "0" and ed == "0":
+        return data
+    else:
+
+        data_ranged = data
+        return data_ranged
 
 
 def graph_json_data(data, ct, bd, ed):
@@ -14,7 +23,7 @@ def graph_json_data(data, ct, bd, ed):
 def api_call(ss, ct, ts, bd, ed):
     if ts == "1":
         url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + ss + \
-              '&interval=5min&apikey=2C4AFL520Q27QCZ9'
+              '&interval=5min&outputsize=compact&apikey=2C4AFL520Q27QCZ9'
     elif ts == "2":
         url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + ss + \
               '&outputsize=full&apikey=2C4AFL520Q27QCZ9'
@@ -25,6 +34,13 @@ def api_call(ss, ct, ts, bd, ed):
     print(url)
     r = requests.get(url)
     data = r.json()
+    json_str = json.dumps(data["Time Series (5min)"])
+
+    type(data)
+    data_df = pd.read_json(json_str)
+    data_df = data_df.T
+    print(data_df)
+    # print(data)
     if 'Error Message' in data:
         print("There was an error in your request, please try again.")
         return 1
@@ -50,9 +66,15 @@ if __name__ == "__main__":
     while 1:
         stock_symbol = "GOOGL"
         chart_type = "2"
-        time_series = "2"
-        begin_date = "1994-08-01"
-        end_date = "1994-08-31'"
+        time_series = "1"
+        if time_series != "2":
+            begin_date = datetime.datetime(2020, 8, 1)
+            end_date = datetime.datetime(2020, 8, 31)
+        else:
+            print('intraday does not support a specified date range, the last two months of intraday values will be'
+                  ' displayed')
+            begin_date = "0"
+            end_date = "0"
 
         ec = api_call(stock_symbol, chart_type, time_series, begin_date, end_date)
         if ec == 0:

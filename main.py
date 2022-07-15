0545dd5d8be8c6bf0a436dd044649cb57285524f
ezmgszi <zaclipperd@gmail.com>
2022-07-15 00:07:50 -0500

    changed the boolean logic of getStockSymbol, was not correctly checking digits in string

--- main.py
+++ main.py
@@ -9,19 +9,18 @@ import pandas as pd
 def getStockSymbol():
 
     while True:
-
         SpecialCharacters = False
         userInput = input("Input a valid stock symbol: ")
         if 0 < len(userInput) <= 5:  #checks if user inputs a string between 1 and 5 characters
             for letter in userInput:
-                if not letter.isdigit() and not letter.isalpha(): #checks if letter isn't a number or a letter
+                if letter.isdigit() or not letter.isalpha(): #checks if letter isn't a number or a letter
                     SpecialCharacters = True
-                    print("please input a symbol with no special characters\n")
+                    print("please input a symbol with no special characters or numbers\n")
                     break
             if not SpecialCharacters:
                 break
         else:
-            print("please input a symbol of 5 characters or less")
+            print("please input a symbol of 5 characters or less\n")
 
     return userInput
 
